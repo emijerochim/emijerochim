d49986c18f4f06a6ff1f7cf475d0f3274949db29
Emi Jerochim <emi.jerochim@gmail.com>
2021-08-24 00:36:06 -0300

    fixing dev

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import underconst from "./underconst.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={underconst} className="App-logo" alt="logo" />
+        <p>Site under construction...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
