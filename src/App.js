520d6bfad0181209e4809e42dac484d6aae1df3d
Emi Jerochim <emi.jerochim@gmail.com>
2021-09-06 11:14:19 -0300

    homepage design

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Banner from "./components/Banner";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
