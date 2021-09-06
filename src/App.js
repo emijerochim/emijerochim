9b75ecdf62a264084558f83e4c6752df65b8282e
Emi Jerochim <emi.jerochim@gmail.com>
2021-09-06 10:54:11 -0300

    homepage design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import underconst from "./underconst.svg";
 import "./App.css";
+import Banner from "./components/Banner";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={underconst} className="App-logo" alt="logo" />
-        <p>Site under construction...</p>
-      </header>
+      <Banner />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./home.scss";
+
+function Home() {
+  return (
+    <>
+      <div className="home-container">
+        <h2>Desarrollador Web</h2>
+        <p>Lleva tu proyecto al siguiente nivel</p>
+        <button>¿Comenzamos?</button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
