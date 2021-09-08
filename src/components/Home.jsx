b5a6eb23cff28fb1badf39a6fecca656dea22e4c
Emi Jerochim <emi.jerochim@gmail.com>
2021-09-07 21:31:50 -0300

    new p

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,11 @@ function Home() {
     <>
       <div className="home-container">
         <h2>Desarrollador Web</h2>
-        <p>Lleva tu proyecto al siguiente nivel</p>
+        <p>
+          Soy un desarrollador web, construyo web apps, bots de discord, landing
+          pages comerciales y más con un stack de tecnologías modernas y código
+          limpio.
+        </p>
         <SocialMedia />
         <img className="arrow" src={arrow} alt="arrow" />
       </div>
