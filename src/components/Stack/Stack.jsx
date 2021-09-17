58b1aa26e0558ac99ee0fd15eef71a5b027cde23
Emi Jerochim <emi.jerochim@gmail.com>
2021-09-17 14:53:55 -0300

    height fix

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,5 +1,6 @@
 import "./stack.scss";
 import StackItem from "./StackItem";
+import Button from "../Button/Button";
 
 import { html5, css3, sass, figma, react } from "../../images/stack/frontend";
 import { node, express } from "../../images/stack/backend";
@@ -11,12 +12,12 @@ function Stack() {
   return (
     <section className="stack" id="stack">
       <div className="container">
-        <h3>Stack</h3>
         <StackItem group="Frontend" items={[html5, css3, sass, figma, react]} />
         <StackItem group="Backend" items={[node, express]} />
         <StackItem group="Databases" items={[mysql, mongodb, postgresql]} />
         <StackItem group="Testing" items={[jestt, testinglibrary]} />
         <StackItem group="Management" items={[git, uml, trello, scrum]} />
+        <Button to="home" />
       </div>
     </section>
   );
