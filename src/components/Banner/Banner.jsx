54f83d118556e8237a9a0e84e51a882683974a01
Emi Jerochim <emi.jerochim@gmail.com>
2021-09-20 19:22:15 -0300

    banner and social media redesign

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import logo from "../../images/logo.png";
 function Banner(props) {
   return (
     <div className={`banner ${props.showMenu}Light`}>
-      <a href="#home">
+      <a className="logo_container" href="#home">
         <img className="logo" src={logo} alt="logo" />
       </a>
       <h1>EMI JEROCHIM</h1>
