660becf56613ec16725105fd6cef1c0b418978fe
emijerochim <emijerochim@gmail.com>
2023-02-17 06:56:22 -0300

    build debug

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
