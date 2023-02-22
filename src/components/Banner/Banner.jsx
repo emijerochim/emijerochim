84ae733672d10bd3123188c39c2bebb5f9dfc6dd
emijerochim <emijerochim@gmail.com>
2023-02-22 08:04:47 -0300

    img update and logo href to home

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import logo from "../../images/logo.png";
 function Banner(props) {
   return (
     <div className={`banner ${props.darkMode}DarkMode`}>
-      <a className="logo_container" href="#home">
+      <a className="logo_container" href="./">
         <img className="logo" src={logo} alt="logo" />
       </a>
       <h1>EMI JEROCHIM</h1>
